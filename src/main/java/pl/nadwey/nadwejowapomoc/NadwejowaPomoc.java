7b513a05a65e56552d2d110973388ba7a9664c48
Nadwey <nadweyyy@gmail.com>
2024-12-17 21:43:06 +0100

    Prevent Farmland from breaking

--- src/main/java/pl/nadwey/nadwejowapomoc/NadwejowaPomoc.java
+++ src/main/java/pl/nadwey/nadwejowapomoc/NadwejowaPomoc.java
@@ -1,17 +1,34 @@
 package pl.nadwey.nadwejowapomoc;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NadwejowaPomoc extends JavaPlugin {
+/**
+ * Notatki:
+ * <ul>
+ * <li>W idealnym świecie handler powinien być w osobnej klasie ale to jest tylko</li>
+ * </ul>
+ */
+public final class NadwejowaPomoc extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
+    @EventHandler
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        if (event.getBlock().getType() == Material.FARMLAND) {
+            event.setCancelled(true);
+        }
 
+        if (event.getEntity() instanceof Player player) {
+            player.sendMessage("Nie możesz1!1!");
+        }
     }
 
 }
